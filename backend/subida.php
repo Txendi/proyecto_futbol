45cb778782d70b45a63f8c47428153ec77670e41
Txendi <endika.mateosbe@elorrieta-errekamari.com>
2026-04-23 22:37:45 +0200

    Ruta de la api cambiada

--- backend/subida.php
+++ backend/subida.php
@@ -3,7 +3,8 @@
 header('Content-Type: application/json');
 
 // Guardamos el Excel directamente en la carpeta data/ de Python
-$directorio = 'C:/xampp/htdocs/ProyectoEibar/ProyectoEibar/data/';
+$directorio = 'C:/xampp/htdocs/ProyectoEibarPhyton/proyecto_Eibar_Phyton/data/';
+
 if (!file_exists($directorio)) {
     mkdir($directorio, 0777, true);
 }
@@ -14,8 +15,8 @@ if (!isset($_FILES['archivo'])) {
     exit;
 }
 
-$archivo   = $_FILES['archivo'];
-$nombre    = basename($archivo['name']);
+$archivo = $_FILES['archivo'];
+$nombre = basename($archivo['name']);
 $rutaFinal = $directorio . $nombre; // Sin timestamp para que Python lo encuentre por nombre exacto
 $extension = strtolower(pathinfo($rutaFinal, PATHINFO_EXTENSION));
 
@@ -30,10 +31,10 @@ if (move_uploaded_file($archivo['tmp_name'], $rutaFinal)) {
     $id_partido = $_POST['id_partido'] ?? null;
 
     echo json_encode([
-        'ok'         => true,
-        'ruta'       => $rutaFinal,
+        'ok' => true,
+        'ruta' => $rutaFinal,
         'id_partido' => $id_partido,
-        'nombre'     => $nombre
+        'nombre' => $nombre
     ]);
 } else {
     echo json_encode(['ok' => false, 'error' => 'Error al guardar el archivo en el servidor']);
